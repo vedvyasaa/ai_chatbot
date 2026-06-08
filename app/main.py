bd54ca3f6870edf0b647c91de9cd1cfcfb4b8daf
VedPrakash Sahu <vedprakashsahu29101999@gmail.com>
2026-06-08 13:00:45 +0530

    Update CORS settings and add sample resume PDF

--- app/main.py
+++ app/main.py
@@ -33,7 +33,7 @@ from app.models.property_model import Property
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["http://localhost:3000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
